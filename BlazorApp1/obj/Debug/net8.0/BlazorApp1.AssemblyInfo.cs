252c20bdf13ee54902e724fb024f998ff59f7673
EfrenLlanes17 <efrenllanes17@gmail.com>
2025-02-21 10:55:11 -0600

    finished Blazor 3

--- BlazorApp1/obj/Debug/net8.0/BlazorApp1.AssemblyInfo.cs
+++ BlazorApp1/obj/Debug/net8.0/BlazorApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8ec8649718534b4ede90a53789603ef6958ce09")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+37fe63aee2dc008ec688a679d93a0ab0f566b97a")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
